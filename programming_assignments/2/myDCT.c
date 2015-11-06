8356adb06550027e6b15ef7fcf5450bf8aaec1a3
Konstantin Macarenco <konstan2@pdx.edu>
2015-11-06 09:02:23 -0800

    Working on pgm Struct

---
+++ programming_assignments/2/myDCT.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+
+struct pgmEncoded {
+    int xDim;
+    int yDim;
+    char header;
+};
+//Input format: myDCT <input image> <quantfile> <qscale> <output file>
+
+int main() {
+    return 0;
+}
